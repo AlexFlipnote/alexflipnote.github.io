5e53c34917130dc9c91f7fa39ff46e0d774e99c9
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2017-05-03 21:15:40 +0200

    Mooore videos!

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,7 +1,8 @@
 const videos = [
-  'TklM2-lSby4', 'iPuTYsiLhzA', 'a0uQnWAmf-k', 'NghF4a58YI0', 'boDSEeGt8hg', 'b8HO6hba9ZE',
-  'y-wtAYBiO9g', 'MLPkKZS6q9k', 'EW86sEEPsTg', 'K5tVbVu9Mkg', 'JbepN4dKLbU', 'G7RgN9ijwE4', '1Bix44C1EzY',
-  'Z8nD8N84Kds', 'SAxpAs1Iaec', 'tKNhPpUR0Pg', 'WvOYHpy4S8s', 'o2TO5atI4rU', 'TPYKwcAyb-o', '5IXQ6f6eMxQ', 'dvyZfa9x3UU'
+  'TklM2-lSby4', 'iPuTYsiLhzA', 'a0uQnWAmf-k', 'NghF4a58YI0', 'boDSEeGt8hg', 'b8HO6hba9ZE', '2rCP4CRRO7E', '4hpEnLtqUDg',
+  'y-wtAYBiO9g', 'MLPkKZS6q9k', 'EW86sEEPsTg', 'K5tVbVu9Mkg', 'JbepN4dKLbU', 'G7RgN9ijwE4', '1Bix44C1EzY', 'gnT8ra9a2eI',
+  'Z8nD8N84Kds', 'SAxpAs1Iaec', 'tKNhPpUR0Pg', 'o2TO5atI4rU', 'TPYKwcAyb-o', '5IXQ6f6eMxQ', 'dvyZfa9x3UU', 'pnj9FAHmgL0',
+  'Zev2vlouV6E', 'NHO84rOp8FQ', 'OGbhJjXl9Rk', 'rY8DSFZ08JQ', '8HlqSrCazqo', '5cEL5mq-OOA', 'OW3q-fjIgAY', 'wsfg5aMRoTQ'
 ];
 
 const randomVideo = videos[Math.floor(Math.random()*videos.length)];
