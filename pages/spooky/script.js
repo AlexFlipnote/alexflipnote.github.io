23aeba61178df7fadc8a5c6de78e8fa9d0817e4f
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-09-26 20:28:06 +0200

    Final spook!

--- pages/spooky/script.js
+++ pages/spooky/script.js
@@ -1,10 +1,48 @@
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("window-titlebar")) {
+    document.getElementById("window-titlebar").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 window.onload = function() {
   var winerr = document.getElementById('window');
   var spookyvid = document.getElementById('spookyvid');
 
+  dragElement(document.getElementById('drag'));
+
   function showvideo() {
     winerr.style.display = "none";
-    spookyvid.style.visibility = "visible";
+    spookyvid.style.display = "block";
     spookyvid.play();
   }
 
