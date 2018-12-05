a79ed1fb7f9c7d59e84b04ec6a86abc1f62892b4
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-12-06 00:25:38 +0100

    Some more changing stuff

--- pages/hexclock/clock.js
+++ pages/hexclock/clock.js
@@ -1,12 +1,14 @@
 function pad(n) {
+  // Converts single digits into 2 digits
   return ("0" + n).slice(-2);
 }
 
 function getTime(hex) {
   var now = new Date();
-  var hour = pad(now.getHours()),
-      minute = pad(now.getMinutes()),
-      second = pad(now.getSeconds());
+
+  var hour = pad(now.getHours());
+  var minute = pad(now.getMinutes());
+  var second = pad(now.getSeconds());
 
   if (hex == "hex") {
     return hour.toString() + minute.toString() + second.toString();
@@ -19,13 +21,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var bg = document.getElementById("bg");
   var time = document.getElementById("time");
   var timetext = document.getElementById("timetext");
-  bg.style.backgroundColor = "#000000";
+  var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
   function timeInHex() {
-    bg.style.backgroundColor = "#" + getTime("hex");
-    time.innerHTML = "#" + getTime("hex");
-    timetext.innerHTML = getTime();
-    setTimeout(timeInHex, 100);
+    gettime = "#" + getTime("hex"); // Lazy HEX maker
+
+    bg.style.backgroundColor = gettime; // Website background
+    time.innerHTML = gettime; // Upper time
+    timetext.innerHTML = getTime(); // Bottom text
+    metaThemeColor.setAttribute("content", gettime); // Change theme for phones
+    document.title = "HEX Clock - " + gettime; // Website title
+
+    setTimeout(timeInHex, 50); // Repeat every 0.05 seconds
   }
 
   timeInHex();
