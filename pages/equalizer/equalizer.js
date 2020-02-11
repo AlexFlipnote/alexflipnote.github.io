348db255f3b2dfc47036ac6a34f0ef1cdfd315dd
Alexander Samuelsen-Manso <alexander.samuelsen-manso@halden.kommune.no>
2020-02-11 13:56:13 +0100

    Cleaning JS code

--- pages/equalizer/equalizer.js
+++ pages/equalizer/equalizer.js
@@ -4,7 +4,7 @@ var wavesurfer = WaveSurfer.create({
   progressColor: '#46B54D'
 })
 
-wavesurfer.load('https://i.alexflipnote.dev/7fzEQP4.mp3');
+wavesurfer.load('https://i.alexflipnote.dev/7fzEQP4.mp3')
 
 
 // Equalizer
@@ -25,79 +25,79 @@ wavesurfer.on('ready', function () {
     {f: 6300, type: 'peaking'},
     {f: 10000, type: 'peaking'},
     {f: 16000, type: 'highshelf'}
-  ];
+  ]
 
   // Create filters
   var filters = EQ.map(function (band) {
-    var filter = wavesurfer.backend.ac.createBiquadFilter();
-    filter.type = band.type;
-    filter.gain.value = 0;
-    filter.Q.value = 1;
-    filter.frequency.value = band.f;
-    return filter;
-  });
+    var filter = wavesurfer.backend.ac.createBiquadFilter()
+    filter.type = band.type
+    filter.gain.value = 0
+    filter.Q.value = 1
+    filter.frequency.value = band.f
+    return filter
+  })
 
   // Connect filters to wavesurfer
-  wavesurfer.backend.setFilters(filters);
+  wavesurfer.backend.setFilters(filters)
 
-  wavesurfer.setVolume(0.5);
-  document.querySelector('#volume').value = wavesurfer.backend.getVolume();
+  wavesurfer.setVolume(0.5)
+  document.querySelector('#volume').value = wavesurfer.backend.getVolume()
 
   // Bind filters to vertical range sliders
-  var container = document.querySelector('#equalizer');
+  var container = document.querySelector('#equalizer')
   filters.forEach(function (filter) {
-    var input = document.createElement('input');
+    var input = document.createElement('input')
     wavesurfer.util.extend(input, {
       type: 'range',
       max: 100,
       min: -25,
       value: 0,
       title: filter.frequency.value
-    });
-    input.style.display = 'inline-block';
-    input.setAttribute('oninput', "this.setAttribute('data-value', this.value / 100)");
-    input.setAttribute('onchange', "this.setAttribute('data-value', this.value / 100)");
-    input.setAttribute('data-value', '0.0');
-    input.setAttribute('orient', 'vertical');
+    })
+    input.style.display = 'inline-block'
+    input.setAttribute('oninput', "this.setAttribute('data-value', this.value / 100)")
+    input.setAttribute('onchange', "this.setAttribute('data-value', this.value / 100)")
+    input.setAttribute('data-value', '0.0')
+    input.setAttribute('orient', 'vertical')
     wavesurfer.drawer.style(input, {
       'webkitAppearance': 'slider-vertical',
       width: '50px',
       height: '150px'
-    });
-    container.appendChild(input);
+    })
+    container.appendChild(input)
 
     var onChange = function (e) {
-      filter.gain.value = ~~e.target.value;
-    };
+      filter.gain.value = ~~e.target.value
+    }
 
-    input.addEventListener('input', onChange);
-    input.addEventListener('change', onChange);
-  });
+    input.addEventListener('input', onChange)
+    input.addEventListener('change', onChange)
+  })
 
 
-    var volumeInput = document.querySelector('#volume');
+    var volumeInput = document.querySelector('#volume')
     var onChangeVolume = function (e) {
-      wavesurfer.setVolume(e.target.value);
-      console.log(e.target.value);
-    };
-  volumeInput.addEventListener('input', onChangeVolume);
-  volumeInput.addEventListener('change', onChangeVolume);
+      wavesurfer.setVolume(e.target.value)
+      console.log(e.target.value)
+    }
+  volumeInput.addEventListener('input', onChangeVolume)
+  volumeInput.addEventListener('change', onChangeVolume)
 
 
   // For debugging
-  wavesurfer.filters = filters;
-});
+  wavesurfer.filters = filters
+})
 
 
 // Let's get those nice values
 function getEqValues() {
-  var getValues = document.querySelector('#equalizer').children;
-  var tempValues = [];
+  var getValues = document.querySelector('#equalizer').children
+  var tempValues = []
   for (var i = 0; i < getValues.length; i++) {
-    var toLavalinkVal = getValues[i].value / 100;
-    if (getValues[i].value == 0) toLavalinkVal = toLavalinkVal.toFixed(1);
-    tempValues.push("("+i+", "+toLavalinkVal+")");
-    getValues[i].setAttribute('data-value', toLavalinkVal);
+    var toLavalinkVal = getValues[i].value / 100
+    if (getValues[i].value == 0) toLavalinkVal = toLavalinkVal.toFixed(1)
+    tempValues.push("("+i+", "+toLavalinkVal+")")
+    getValues[i].setAttribute('data-value', toLavalinkVal)
   }
-  document.querySelector('#eq-values').innerHTML = tempValues.join(", ");
+  document.querySelector('#eq-values').innerHTML = tempValues.join(", ")
 }
--- pages/spooky/script.js
+++ pages/spooky/script.js
@@ -1,69 +1,69 @@
 window.onload = function() {
-  var winerr = document.getElementById('window');
-  var spookyvid = document.getElementById('spookyvid');
-  var spookwindow = document.getElementById('spookwindow');
-  var dankmode = document.getElementById('dankmode');
-  var drag = document.getElementById("drag");
+  var winerr = document.getElementById('window')
+  var spookyvid = document.getElementById('spookyvid')
+  var spookwindow = document.getElementById('spookwindow')
+  var dankmode = document.getElementById('dankmode')
+  var drag = document.getElementById("drag")
 
-  var element = document.createElement("source");
-  element.type = "video/mp4";
+  var element = document.createElement("source")
+  element.type = "video/mp4"
 
-  dragElement(drag, "-titlebar");
+  dragElement(drag, "-titlebar")
 
   function showvideo() {
     if (dankmode.style.display !== "none") {
-      element.src = "video.mp4";
+      element.src = "video.mp4"
     }
 
-    spookyvid.appendChild(element);
-    winerr.style.display = "none";
-    dankmode.style.display = "none";
-    spookwindow.style.display = "block";
-    spookyvid.play();
-    addTimer();
+    spookyvid.appendChild(element)
+    winerr.style.display = "none"
+    dankmode.style.display = "none"
+    spookwindow.style.display = "block"
+    spookyvid.play()
+    addTimer()
   }
 
   function createClone() {
-    var min = 1;
-    var max = 80;
+    var min = 1
+    var max = 80
 
-    var clone = drag.cloneNode(true);
+    var clone = drag.cloneNode(true)
 
-    clone.id = drag.id;
-    clone.style.top = Math.floor(Math.random() * (max - min + 1)) + min + "%";
-    clone.style.left = Math.floor(Math.random() * (max - min + 1)) + min + "%";
+    clone.id = drag.id
+    clone.style.top = Math.floor(Math.random() * (max - min + 1)) + min + "%"
+    clone.style.left = Math.floor(Math.random() * (max - min + 1)) + min + "%"
 
-    var doot = new Audio('doot.mp3');
-    doot.play();
+    var doot = new Audio('doot.mp3')
+    doot.play()
 
-    winerr.appendChild(clone);
-    dragElement(clone, null);
+    winerr.appendChild(clone)
+    dragElement(clone, null)
   }
 
   document.getElementById('dank').onclick = function() {
-    element.src = "video_dank.mp4";
-    dankmode.style.display = "none";
+    element.src = "video_dank.mp4"
+    dankmode.style.display = "none"
   }
 
   document.body.onclick = function (ev) {
-    if (ev.target.getAttribute("id") == "window-close") { createClone(); }
-    if (ev.target.getAttribute("id") == "spooktime") { showvideo(); }
-    if (ev.target.getAttribute("id") == "spooktime2") { showvideo(); }
-  };
+    if (ev.target.getAttribute("id") == "window-close") { createClone() }
+    if (ev.target.getAttribute("id") == "spooktime") { showvideo() }
+    if (ev.target.getAttribute("id") == "spooktime2") { showvideo() }
+  }
 
   // Prevent shit
   // Right-click menu
-  document.addEventListener('contextmenu', event => event.preventDefault());
+  document.addEventListener('contextmenu', event => event.preventDefault())
 
   // Deleting popup
   window.setInterval(function() {
     if (document.getElementById("window") == null) {
-      location.reload();
+      location.reload()
     }
 
     if (document.getElementsByClassName("window").length == 0) {
-      createClone();
-      console.log("Nice try.. but you can't delete it that simply");
+      createClone()
+      console.log("Nice try.. but you can't delete it that simply")
     }
-  }, 1000);
+  }, 1000)
 }
