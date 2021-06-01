ad64ec774ab833af4ee61d6c7ddcc6cf4a448d53
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2021-06-01 01:03:08 +0000

    Deploying to gh-pages from @ AlexFlipnote/alexflipnote.github.io@6e129be88dacf8b768f41e0b64da271e6808fb2d 🚀

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,12 @@ function scroll_to(get_id) {
 window.addEventListener('scroll', function() {
   // Hide arrow button when scrolling down the page
   var scrollPosition = window.scrollY
-  var logoContainer = document.getElementsByClassName('arrow')[0]
-  if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled')
-  else logoContainer.classList.remove('arrow--scrolled')
+  var findLogoContainer = document.getElementsByClassName('arrow')
+  if (findLogoContainer.length > 0) {
+    var logoContainer = findLogoContainer[0]
+    if (scrollPosition >= 100) logoContainer.classList.add('arrow--scrolled')
+    else logoContainer.classList.remove('arrow--scrolled')
+  }
 })
 
 document.addEventListener('DOMContentLoaded', function() {
