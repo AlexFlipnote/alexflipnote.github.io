a6ce1de3a54cf54a19efa855e0df4484082d844d
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-09-11 21:44:54 +0200

    Updating to better website

--- index.py
+++ index.py
@@ -1,11 +1,34 @@
 import sys
+import json
 
 from jinja2_static import Builder
 
 build = Builder()
 argument = sys.argv[1] if len(sys.argv) > 1 else None
 
+with open("./data.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+
+def branding_builder(name: str):
+    brand_data = data["branding_images"][name]
+
+    download_links = "\n".join([
+        f'<a download="{name}" href="/static/images/branding/{name}.{g}" target="_blank">.{g}</a>'
+        for g in brand_data["formats"]
+    ])
+
+    return f"""
+        <img src="/static/images/branding/{name}.{brand_data['formats'][0]}">
+        <span class="image-text">{brand_data['name']}</span>
+        <div class="image-links">
+            {download_links}
+        </div>
+    """
+
+
 build.generate(
     debug=False if argument == "build" else True,
-    URL="https://alexflipnote.dev/"
+    URL="https://alexflipnote.dev/", data=data,
+    branding_builder=branding_builder
 )
