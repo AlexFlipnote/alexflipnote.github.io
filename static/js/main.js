1de67e87f21cedd1090f77dd24ff64b7bc9972a5
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-12-17 01:25:18 +0100

    Cleaning the JS

--- static/js/main.js
+++ static/js/main.js
@@ -3,11 +3,8 @@ function christmas_light_pos(scroll_y) {
   if (!clights) return
 
   const scroll_y_limit = 450
-  if (scroll_y <= scroll_y_limit) {
-    clights.style.margin = `${-150 + (scroll_y / 3.5)}px 0 0 0`
-  } else {
-    clights.style.margin = `${-150 + (scroll_y_limit / 3.5)}px 0 0 0`
-  }
+  if (scroll_y <= scroll_y_limit) clights.style.margin = `${-150 + (scroll_y / 3.5)}px 0 0 0`
+  else clights.style.margin = `${-150 + (scroll_y_limit / 3.5)}px 0 0 0`
 
   if (scroll_y <= (window.innerHeight - 12)) {
     clights.style.position = "absolute"
@@ -25,7 +22,6 @@ function calc_years_coding() {
 
   const years_since = new Date("2015-08-01").getTime()
   const today = new Date().getTime()
-
   const years = Math.floor(
     (today - years_since) / (1000 * 60 * 60 * 24 * 365)
   )
@@ -42,27 +38,17 @@ function spin_video() {
   spin.addEventListener("click", () => {
     video.play()
     spin.remove()
-    let starttime = Date.now()
+    const starttime = Date.now()
 
     window.setInterval(function() {
-      let s = (Date.now() - starttime)/1000
+      let s = (Date.now() - starttime) / 1000
       let str = ""
-      if (s > 86400) {
-        str = Math.floor(s/86400) + "d "
-        s %= 86400
-      }
 
-      if (s > 3600) {
-        str += Math.floor(s/3600) + "h "
-        s %= 3600
-      }
-
-      if (s > 60) {
-        str += Math.floor(s/60) + "m "
-        s %= 60
-      }
-
+      if (s > 86400) str = Math.floor(s/86400) + "d "; s %= 86400
+      if (s > 3600) str += Math.floor(s/3600) + "h "; s %= 3600
+      if (s > 60)str += Math.floor(s/60) + "m "; s %= 60
       str += s.toFixed(2) + "s"
+
       time.textContent = str
     }, 10)
 
@@ -80,50 +66,13 @@ window.addEventListener("scroll", () => {
   christmas_light_pos(scroll_y)
 })
 
-async function paste_discord_info() {
-  const discord_loading = document.getElementById("discord-status-loading")
-  const discord_avatar = document.getElementById("discord-avatar")
-  const discord_status = document.getElementById("discord-status")
-  const discord_status_name = document.getElementById("discord-status-name")
-  const discord_spotify = document.getElementById("discord-spotify")
-  const discord_status_circle = document.getElementById("discord-status-circle")
-  const spotify_image = document.getElementById("spotify-image")
-  const spotify_title = document.getElementById("spotify-title")
-  const spotify_url = document.getElementById("spotify-url")
-  if (!discord_avatar) return
-
-  discord_loading.style.display = "none"
-
-  const discord = await new UserInfo("86477779717066752").fetch()
-  discord_avatar.src = discord.avatar_url()
-
-  discord_status_circle.style.backgroundColor = `#${discord.status_hex()}`
-
-  if (discord.spotify.title) {
-    discord_status.style.display = "none"
-    discord_spotify.style.display = "flex"
-    spotify_image.src = discord.spotify.image_url
-    spotify_title.innerText = `Listening to spotify:\n${discord.spotify.title}`
-    spotify_title.style.color = `#${discord.status_hex()}`
-    spotify_url.href = discord.spotify.url
-  } else {
-    discord_spotify.style.display = "none"
-    discord_status.style.display = "block"
-    discord_status_name.style.color = `#${discord.status_hex()}`
-    if (discord.status_text()) {
-      discord_status_name.innerText = `${discord.status_text()}`
-    } else {
-      discord_status_name.innerText = `Not doing anything`
-    }
-  }
-}
-
 document.addEventListener("DOMContentLoaded", async () => {
-  // Load essentials
+  // Load essentials (this file)
   calc_years_coding()
   spin_video()
   christmas_light_pos(window.scrollY)
 
+  // ./static/js/discord.js
   paste_discord_info()
   window.setInterval(paste_discord_info, 5000)
 })
