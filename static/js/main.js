75c722138f7dd1fdaeaa8e121cbb27629e546841
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2023-02-11 03:46:57 +0000

    Deploying to gh-pages from @ AlexFlipnote/alexflipnote.github.io@21bc55d50a2e9ad710c7dc407d9aaca4b5fb06c1 🚀

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,3 @@
-function christmas_light_pos(scroll_y) {
-  const clights = document.getElementById("lightrope")
-  if (!clights) return
-
-  const scroll_y_limit = 450
-  if (scroll_y <= scroll_y_limit) clights.style.margin = `${-150 + (scroll_y / 3.5)}px 0 0 0`
-  else clights.style.margin = `${-150 + (scroll_y_limit / 3.5)}px 0 0 0`
-
-  if (scroll_y <= (window.innerHeight - 12)) {
-    clights.style.position = "absolute"
-    clights.style.top = "inherit"
-  } else {
-    clights.style.position = "fixed"
-    clights.style.margin = "-10px 0 0 0"
-    clights.style.top = "0"
-  }
-}
-
 function calc_years_coding() {
   const timestamp_text = document.getElementById("years-of-coding")
   if (!timestamp_text) return
@@ -62,17 +44,10 @@ window.addEventListener("scroll", () => {
 
   if (scroll_y >= 100) arrow.classList.add("arrow--scrolled")
   else arrow.classList.remove("arrow--scrolled")
-
-  christmas_light_pos(scroll_y)
 })
 
 document.addEventListener("DOMContentLoaded", async () => {
   // Load essentials (this file)
   calc_years_coding()
   spin_video()
-  christmas_light_pos(window.scrollY)
-
-  // ./static/js/discord.js
-  paste_discord_info()
-  window.setInterval(paste_discord_info, 5000)
 })
