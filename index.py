fe45861ca2cf51187fd4d0bfe8da6511ec950849
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2024-10-28 11:55:01 +0100

    Update personal info

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from jinja2_static import Builder
 build = Builder()
 argument = sys.argv[1] if len(sys.argv) > 1 else None
 
-with open("./data.json", "r", encoding="utf-8") as f:
+with open("./static/data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 
